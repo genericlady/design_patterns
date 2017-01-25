b5eea2671320157785e5af86d7f93a83e4c88db1
yianna <yianna@thoughtbot.com>
2017-01-25 11:38:15 -0500

    Implement Observer pattern

--- chapter02_observer/weather_data/weather_data.rb
+++ chapter02_observer/weather_data/weather_data.rb
@@ -1,15 +1,80 @@
-class WeatherData
+class Subject
+  def register_observer(observer)
+    raise "Implement a method to push an observer into an array"
+  end
+
+  def remove_observer(observer)
+    raise "Implement a method to remove a specific observer from the array"
+  end
+
+  def notify_observers
+    raise "Iterate through each observer and update each one"
+  end
+end
+
+class WeatherData < Subject
   def initialize
-    @declarations
+    @observers = []
+    @temperature
+    @humidity
+    @pressure
+  end
+
+  def register_observer(observer)
+    @observers << observer
+  end
+
+  def remove_observer(observer)
+    @observers.remove(observer)
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.update(@temperature, @humidity, @pressure)
+    end
+  end
+
+  def set_measurements(temperature, humidity, pressure)
+    @humidity = get_humidity
+    @pressure = get_pressure
+    @temp     = get_temperature
+    
+    measurements_changed
   end
 
   def measurements_changed
-    temp = get_temperature
-    humidity = get_humidity
-    pressure = get_pressure
+    notify_observers
+  end
 
-    current_conditions_display.update(temp, humidity, pressure)
-    statistics_display.update(temp, humidity, pressure)
-    forecast_display.update(temp, humidity, pressure)
+end
+
+module Observer
+  def update(temp, humidity, pressure)
+  end
+end
+
+module DisplayElement
+  def display
+  end
+end
+
+class CurrentConditionsDisplay
+  include Observer, DisplayElement
+
+  def initialize(weather_data)
+    @temperature
+    @humidity
+    @weather_data = weather_data
+    @weather_data.register_observer(self)
+  end
+
+  def update(temperature, humidity)
+    @temperature = temperature
+    @humidity = humidity
+  end
+
+  def display
+    puts "Current conditions: " + temperature
+      + "F degrees and " + humidity + "% humidity"
   end
 end
